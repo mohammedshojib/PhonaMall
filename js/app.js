96dbfca07f237f692cd43c0400ca6d16aef3f4be
mohammedshojib <asmdshojib11@gmail.com>
2022-03-01 17:23:37 +0600

    error handled

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 const buttonSearch = () => {
-  const searchValue = document.getElementById("searchArea").value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+  const searchValue = document.getElementById("searchArea");
+  const searchVal = searchValue.value;
+  searchValue.value = "";
+  const parentDiv = document.getElementById("allPhone");
+  parentDiv.textContent = "";
+  if (isNaN(searchVal) === false) {
+    searchValue.setAttribute("placeHolder", "please enter a phone name");
+    alert("please enter a phone name");
+    //     const parentDiv = document.getElementById("allPhone");
+    //     const div = document.createElement("div");
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((myData) => showPhones(myData.data));
+    //     div.innerHTML = `
+    //     <div class="alert alert-danger" role="alert">
+    //     please enter a coreect phone name!
+    // </div>`;
+    //     parentDiv.appendChild(div);
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchVal}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((myData) => showPhones(myData.data));
+  }
 };
 
 const showPhones = (phones) => {
   for (const phone of phones) {
     const parentDiv = document.getElementById("allPhone");
     const div = document.createElement("div");
+
     div.innerHTML = `
     <div class="col">
       <div class="card text-center">
@@ -18,8 +35,12 @@ const showPhones = (phones) => {
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <h6 class=""><strong>brand: ${phone.brand}</strong></h6>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <button class="btn btn-primary" onclick="details('${phone.slug}')">Details</button>
+          <p class="card-text">${
+            phone.releaseDate || "Releas Date not found"
+          }</p>
+          <button class="btn btn-primary" onclick="details('${
+            phone.slug
+          }')">Details</button>
           <button class="btn btn-danger">Danger</button>
         </div>
         
@@ -39,16 +60,26 @@ const details = (id) => {
 const detailsPopUp = (info) => {
   console.log(info);
   const hello = document.getElementById("detailsContainer");
+  hello.textContent = "";
   const div = document.createElement("div");
   div.innerHTML = `
     <div class="col">
       <div id="detailsContainer" class="card text-center">
-        <img src="${info.image}" class="card-img-top" style="width: 50%;" alt="...">
+        <img src="${
+          info.image
+        }" class="card-img-top" style="width: 50%;" alt="...">
         <div class="card-body">
           <h5 class="card-title">${info.name}</h5>
-          <h6 class=""><strong>ChipSet: ${info.mainFeatures.chipSet}</strong></h6>
+          <h6 class=""><strong>ChipSet: ${
+            info.mainFeatures.chipSet || "Device Dosn't have ChipSet"
+          }</strong></h6>
           <p class="card-text">${info.mainFeatures.memory}</p>
-          <p class="card-text">${info.mainFeatures.sensors}</p>
+          <p class="card-text">${
+            info.mainFeatures.sensors || "Sensor is not found"
+          }</p>
+          <p class="card-text">${
+            info.mainFeatures.releaseDate || "Releas Date not found"
+          }</p>
         
         </div>
         
